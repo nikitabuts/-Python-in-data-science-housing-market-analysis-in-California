aec89d6af8b0e85c6a024aee5338177a012b58d7
nikitabuts <nikitabuts2000@gmail.com>
2020-01-20 16:22:36 +0000

    Second version

--- py_files/adding_window.py
+++ py_files/adding_window.py
@@ -12,47 +12,47 @@ class AddingWindow(tk.Toplevel):
         super().__init__()
         self.master = master
         self.window = window
-        self.classes = ['INLAND', 'NEAR BAY', '<1H OCEAN', 'NEAR OCEAN', 'ISLAND']
+        self.classes = self.master.dataset['ocean_proximity'].unique()
         self.flag_prediction1 = False
         self.flag_prediction2 = False
 
-        self.longitude_label = tk.Label(self, width=30, text='Введите параметр longitude', anchor='w')
+        self.longitude_label = tk.Label(self, width=30, text='Введите longitude', anchor='w')
         self.longitude_entry = tk.Spinbox(self, width=30, from_=np.min(self.master.dataset.iloc[:, 0]),
                                           to=np.max(self.master.dataset.iloc[:, 0]))
 
-        self.latitude_label = tk.Label(self, width=30, text='Введите параметр latitude', anchor='w')
+        self.latitude_label = tk.Label(self, width=30, text='Введите latitude', anchor='w')
         self.latitude_entry = tk.Spinbox(self, width=30, from_=np.min(self.master.dataset.iloc[:, 1]),
                                          to=np.max(self.master.dataset.iloc[:, 1]))
 
-        self.housing_median_age_label = tk.Label(self, width=30, text='Введите параметр housing_median_age', anchor='w')
+        self.housing_median_age_label = tk.Label(self, width=30, text='Введите housing_median_age', anchor='w')
         self.housing_median_age_entry = tk.Spinbox(self, width=30, from_=np.min(self.master.dataset.iloc[:, 2]),
                                                    to=np.max(self.master.dataset.iloc[:, 2]))
 
-        self.total_rooms_label = tk.Label(self, width=30, text='Введите параметр total_rooms', anchor='w')
+        self.total_rooms_label = tk.Label(self, width=30, text='Введите total_rooms', anchor='w')
         self.total_rooms_entry = tk.Spinbox(self, width=30, from_=np.min(self.master.dataset.iloc[:, 3]),
                                             to=np.max(self.master.dataset.iloc[:, 3]))
 
-        self.total_bedrooms_label = tk.Label(self, width=30, text='Введите параметр total_bedrooms', anchor='w')
+        self.total_bedrooms_label = tk.Label(self, width=30, text='Введите total_bedrooms', anchor='w')
         self.total_bedrooms_entry = tk.Spinbox(self, width=30, from_=np.min(self.master.dataset.iloc[:, 4]),
                                                to=np.max(self.master.dataset.iloc[:, 4]))
 
-        self.population_label = tk.Label(self, width=30, text='Введите параметр population', anchor='w')
+        self.population_label = tk.Label(self, width=30, text='Введите population', anchor='w')
         self.population_entry = tk.Spinbox(self, width=30, from_=np.min(self.master.dataset.iloc[:, 5]),
                                            to=np.max(self.master.dataset.iloc[:, 5]))
 
-        self.households_label = tk.Label(self, width=30, text='Введите параметр households', anchor='w')
+        self.households_label = tk.Label(self, width=30, text='Введите households', anchor='w')
         self.households_entry = tk.Spinbox(self, width=30, from_=np.min(self.master.dataset.iloc[:, 6]),
                                            to=np.max(self.master.dataset.iloc[:, 6]))
 
-        self.median_income_label = tk.Label(self, width=30, text='Введите параметр median_income', anchor='w')
+        self.median_income_label = tk.Label(self, width=30, text='Введите median_income', anchor='w')
         self.median_income_entry = tk.Spinbox(self, width=30, from_=np.min(self.master.dataset.iloc[:, 7]),
                                               to=np.max(self.master.dataset.iloc[:, 7]))
 
-        self.median_house_value_label = tk.Label(self, width=30, text='Введите параметр median_house_value', anchor='w')
+        self.median_house_value_label = tk.Label(self, width=30, text='Введите median_house_value', anchor='w')
         self.median_house_value_entry = tk.Spinbox(self, width=30, from_=np.min(self.master.dataset.iloc[:, 9]),
                                                    to=np.max(self.master.dataset.iloc[:, 9]))
 
-        self.ocean_proximity_label = tk.Label(self, width=30, text='Введите параметр ocean_proximity', anchor='w')
+        self.ocean_proximity_label = tk.Label(self, width=30, text='Введите ocean_proximity', anchor='w')
         self.ocean_proximity_entry = tk.Listbox(self, width=30, height=6, selectmode=tk.SINGLE)
 
         self.price_predict_label = tk.Label(self, width=30, text='Хотите ли вы, чтобы цена квартиры\n'
@@ -125,21 +125,24 @@ class AddingWindow(tk.Toplevel):
             flag = False
             messagebox.showinfo("Ошибка!", "Не указан параметр ocean_proximity_entry!")
         else:
-            dict = {
-                0: self.longitude_entry.get(),
-                1: self.latitude_entry.get(),
-                2: self.housing_median_age_entry.get(),
-                3: self.total_bedrooms_entry.get(),
-                4: self.total_bedrooms_entry.get(),
-                5: self.population_entry.get(),
-                6: self.households_entry.get(),
-                7: self.median_income_entry.get(),
-                8: self.classes[self.ocean_proximity_entry.curselection()[0]],
-                9: self.median_house_value_entry.get(),
-            }
-            for i in [0, 1, 2, 3, 4, 5, 6, 7, 9]:
+            dictionary = dict(zip(np.arange(0, 10).tolist(),
+                            [self.longitude_entry.get(),
+                                 self.latitude_entry.get(),
+                                 self.housing_median_age_entry.get(),
+                                 self.total_bedrooms_entry.get(),
+                                 self.total_bedrooms_entry.get(),
+                                 self.population_entry.get(),
+                                 self.households_entry.get(),
+                                 self.median_income_entry.get(),
+                                 self.classes[self.ocean_proximity_entry.curselection()[0]],
+                                 self.median_house_value_entry.get()]))
+
+            for i in range(0, 10):
                 try:
-                    a = float(dict[i])
+                    if i != 8:
+                        variable = float(dictionary[i])
+                    else:
+                        variable = dictionary[i]
                 except ValueError:
                     flag = False
                     messagebox.showinfo("Ошибка!", "Неверный тип переменной " + str(self.master.dataset.columns[i]) + "!")
@@ -149,7 +152,7 @@ class AddingWindow(tk.Toplevel):
                 messagebox.showinfo("Ошибка!", "Не указан параметр ocean_proximity_entry!")
 
             if flag:
-                data_frame = self.appending(dict)
+                data_frame = self.appending(dictionary)
                 self.master.dataset = self.master.dataset.append(data_frame, ignore_index=True)
                 matrix = data_frame.values
                 cells = []
@@ -172,96 +175,109 @@ class AddingWindow(tk.Toplevel):
                 x = self.master.dataset.drop(['median_house_value', 'ocean_proximity'], axis=1)
                 y = self.master.dataset.median_house_value
                 auxiliary_sub_array = auxiliary_array[:-1].astype(float)
-                weights = regression_weights(x, y)
-                variable = int(predict(auxiliary_sub_array, regression_weights(x, y)))
+
+                regression = LinearRegression()
+                regression.fit(x, y)
+                variable = int(regression.predict(auxiliary_sub_array))
+
             elif columns_of_dataset[iteration] == 'ocean_proximity' and self.flag_prediction2:
                 auxiliary_sub_array = [auxiliary_array[0:2]]
-                variable = classificator(self.master.dataset, auxiliary_sub_array)
+                classificator = MultiClassification(self.master.dataset)
+                classificator.fit()
+                variable = classificator.predict(auxiliary_sub_array)
             else:
                 variable = dict[iteration]
 
             auxiliary_array = np.append(auxiliary_array, variable)
 
         df = pd.DataFrame(columns=self.master.dataset.columns)
+
         df.loc[len(self.master.dataset)] = [float(auxiliary_array[0]), float(auxiliary_array[1]),
                                             float(auxiliary_array[2]), float(auxiliary_array[3]),
                                             float(auxiliary_array[4]), float(auxiliary_array[5]),
                                             float(auxiliary_array[6]), float(auxiliary_array[7]),
                                             (auxiliary_array[8]), float(auxiliary_array[9])]
+
         return df
 
 
-def class_balancing(dataset):
-    helped_dataset = dataset.copy()
-    helped_dataset.ocean_proximity, d = helped_dataset.ocean_proximity.factorize()
-    series = pd.Series(helped_dataset.ocean_proximity)
-    maximum = series.value_counts().max()
+class MultiClassification:
+    def __init__(self, dataset):
+        self.__dataset = dataset
 
-    x = pd.DataFrame(columns=['longitude', 'latitude', 'ocean_proximity']).values
-    for i in np.arange(len(series.unique())):
-        helped_2 = series.unique()[i]
-        array = helped_dataset[helped_dataset['ocean_proximity'] == helped_2].loc[:,
-                ['longitude', 'latitude', 'ocean_proximity']].values.reshape(-1, 3)
-        help_3 = array.copy()
-        while len(array) < maximum:
-            if maximum - len(array) >= len(help_3):
-                array = np.append(array, help_3, axis=0)
-            else:
-                array = np.append(array, help_3[0: maximum - len(help_3)], axis=0)
-        x = np.append(x, array, axis=0)
+    def __class_balancing(self):
+        helped_dataset = self.__dataset.copy()
+        helped_dataset.ocean_proximity, _ = helped_dataset.ocean_proximity.factorize()
+        series = pd.Series(helped_dataset.ocean_proximity)
+        maximum = series.value_counts().max()
 
-    x = pd.DataFrame(x, columns=['longitude', 'latitude', 'ocean_proximity'])
-    return x
+        x = pd.DataFrame(columns=['longitude', 'latitude', 'ocean_proximity']).values
 
+        for i in np.arange(len(series.unique())):
+            helped_2 = series.unique()[i]
+            array = helped_dataset[helped_dataset['ocean_proximity'] == helped_2].loc[:,
+                    ['longitude', 'latitude', 'ocean_proximity']].values.reshape(-1, 3)
+            help_3 = array.copy()
+            while len(array) < maximum:
+                if maximum - len(array) >= len(help_3):
+                    array = np.append(array, help_3, axis=0)
+                else:
+                    array = np.append(array, help_3[0: maximum - len(help_3)], axis=0)
+            x = np.append(x, array, axis=0)
 
-def classificator(dataset, array):
-    balanced_dataset = class_balancing(dataset)
-    variable = len(balanced_dataset)
+        x = pd.DataFrame(x, columns=['longitude', 'latitude', 'ocean_proximity'])
+        return x
 
-    value = dataset.ocean_proximity.unique()
-    y = pd.DataFrame(balanced_dataset).iloc[:, 2].values.reshape((variable,)).astype(int)
-    x = balanced_dataset.drop('ocean_proximity', axis=1).values.astype(float)
+    def fit(self):
+        balanced_dataset = self.__class_balancing()
 
-    skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
-    for train_index, test_index in skf.split(x, y):
-        x_train, x_test = x[train_index], x[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+        self.y = pd.DataFrame(balanced_dataset).iloc[:, 2].values.reshape((len(balanced_dataset),)).astype(int)
+        self.x = balanced_dataset.drop('ocean_proximity', axis=1).values.astype(float)
 
-    clf = RandomForestClassifier(n_estimators=500, max_depth=20)
-    clf.fit(x_train, y_train)
-    prediction = clf.predict(array)
-    y_pred = clf.predict(x_test)
-    z = pd.Series(y).unique()
-    for i in range(len(value)):
-        if prediction == z[i]:
-            prediction = value[i]
-    return prediction
+        skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
 
+        for train_index, test_index in skf.split(self.x, self.y):
+            x_train, x_test = self.x[train_index], self.x[test_index]
+            y_train, y_test = self.y[train_index], self.y[test_index]
 
-def regression_weights(x, y):
-    x = np.append(np.ones((x.shape[0], 1)), x, axis=1)
-    weights = np.linalg.inv(x.T @ x) @ x.T @ y.values
-    return weights
+        self.clf = RandomForestClassifier(n_estimators=500, max_depth=20)
+        self.clf.fit(x_train, y_train)
 
-
-def predict(x, weights):  # Не работает - доделать
-    b = weights[0]
-    weights = weights[1: len(weights)]
-    predictions = b + x @ weights.T
-    return predictions
+    def predict(self, x_test):
+        y_pred = self.clf.predict(x_test)
+        labels_unique = pd.Series(self.y).unique()
+        preds = self.clf.predict(x_test)
+        for i in range(len(labels_unique)):
+            if preds == labels_unique[i]:
+                preds = self.__dataset.ocean_proximity.unique()[i]
+        return preds
 
 
-def score(y_test, y_pred):
-    d = np.abs(y_test - y_pred)
-    epsilons = list(map(float, input().split()))
-    scores = np.array([])
-    for i in np.arange(len(epsilons)):
-        summary = 0
-        for j in np.arange(len(d)):
-            if d[j] <= epsilons[i]:
-                summary += 1
-        score = summary / len(d)
-        scores = np.append(scores, score)
-    scoresDF = pd.DataFrame({"Epsilon": epsilons, "Score": scores})
+class LinearRegression:
+    def __init__(self):
+        self.weights = np.array([])
 
-    return scoresDF
+    def fit(self, x, y):
+        x = np.append(np.ones((x.shape[0], 1)), x, axis=1)
+        self.weights = np.linalg.inv(x.T @ x) @ x.T @ y.values
+
+    def predict(self, x):
+        bias = self.weights[0]
+        self.weights = self.weights[1: len(self.weights)]
+        predictions = bias + x @ self.weights.T
+        return predictions
+
+    @staticmethod
+    def score(y_test, y_pred):
+        absolute_error = np.abs(y_test - y_pred)
+        epsilons = list(map(float, input().split()))
+        scores = np.array([])
+        for i in np.arange(len(epsilons)):
+            summary = 0
+            for j in np.arange(len(absolute_error)):
+                if absolute_error[j] <= epsilons[i]:
+                    summary += 1
+            score = summary / len(absolute_error)
+            scores = np.append(scores, score)
+        scores_df = pd.DataFrame({"Epsilon": epsilons, "Score": scores})
+        return scores_df
